3d9ae7b6fbd5875638fe464f81a50f3812e99b24
jsabbath <jsabbath@asu.edu>
2019-01-30 22:36:07 -0700

    Added saveMetadata to attempt metadata transmission

--- build_automation/frontend/src/js/bulk_metadata_upload.js
+++ build_automation/frontend/src/js/bulk_metadata_upload.js
@@ -4,6 +4,9 @@ import TextField from '@material-ui/core/TextField';
 import {withStyles} from '@material-ui/core/styles';
 import {creatMuiTheme} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import {APP_URLS, get_url} from "./url";
+import Snackbar from '@material-ui/core/Snackbar';
+import axios from 'axios';
  
 const style = theme => ({
     root: {
@@ -66,6 +69,83 @@ class BulkMetadataUpload extends React.Component {
                 return newState;
             });
         }
+        //////ADDED SAVE AND IS_VALID FROM UPLOAD CONTENTS TO ATTEMPT TRANSMISSION OF  METADATA ::: BUSTED
+        saveMetadata(evt) {
+            /*if (!this.is_valid_state(!(this.state.id > 0))) {
+			console.log("invalid state bro!!!!: " + this.state.id);
+			// If it is in an invalid state, do not proceed with the save operation.
+            return;
+            }*/
+            
+            var targetUrl = APP_URLS.CONTENTS_LIST;
+            
+            Array.from(this.state.contentFile).forEach(file => {
+                console.log("calling method: " + file.name);
+                const payload = new FormData();
+                var currentDate = new Date();
+                Boolean(file) && payload.append('content_file', file);
+                payload.append('name', file.name);
+                payload.append('description', "Please insert a description.");
+                payload.append('updated_time', this.formatDate(currentDate));
+                const currInstance = this;
+                if (this.state.id > 0) {
+                    // Update an existing directory.
+                    payload.append('id', this.state.id);
+                    targetUrl = get_url(APP_URLS.CONTENT_DETAIL, {id:this.state.id});
+                    axios.patch(targetUrl, payload, {
+                        responseType: 'json'
+                    }).then(function(response) {
+                        currInstance.saveCallback(response.data, true);
+                    }).catch(function(error) {
+                        console.error("Error in updating the content", error);
+                        console.error(error.response.data);
+                        let errorMsg = 'Error in updating the content';
+                        currInstance.setState({
+                            message: errorMsg,
+                            messageType: 'error'
+                        });
+                    });
+                } else {
+                    // Create a new directory.
+                    axios.post(targetUrl, payload, {
+                        responseType: 'json'
+                    }).then(function(response) {
+                        currInstance.saveCallback(response.data, false);
+                    }).catch(function(error) {
+                        console.error("Error in uploading the content", error);
+                        console.error(error.response.data);
+                        let errorMsg = 'Error in uploading the content';
+                        currInstance.setState({
+                            message: errorMsg,
+                            messageType: 'error'
+                        });
+                    });
+                }
+            });
+        }
+        
+        is_valid_state(is_save) {
+        var hasErrors = false;
+        const fieldErrors = {};
+		console.log(this.state);
+        /* if (!this.state.name || this.state.name.trim().length === 0) {
+            hasErrors = true;
+            fieldErrors['name'] = 'Name is required.';
+        } */
+        /* if (!this.state.description || this.state.description.trim().length === 0) {
+            hasErrors = true;
+            fieldErrors['description'] = 'Description is required.';
+        } */
+		if(!this.state.contentFileNames.length > 0) {
+			hasErrors = true;
+			fieldErrors['name'] = 'File names are required.';
+		}
+		
+        if (hasErrors) {
+            this.setState({fieldErrors});
+        }
+        return !hasErrors;
+    }
         
 		render(){
 				return (
@@ -98,7 +178,7 @@ class BulkMetadataUpload extends React.Component {
 								</Button>
 								</label>
                                 <div style={{marginTop: '20px'}}> </div>
-                                <Button variant="contained" component="span" onClick={this.saveContent}>
+                                <Button variant="contained" component="span" onClick={this.saveMetadata}>
                                     Save
                                 </Button>
                                 </Grid>
